b40004774461a4ccd69db3e62d0fc5603ad6c862
Luan Victor <106849037+LottaFeroce@users.noreply.github.com>
2025-04-15 21:33:12 -0400

    Create progre_arit.py
    matematica prg arit

---
+++ progre_arit.py
@@ -0,0 +1,100 @@
+"""
+Progressão Aritmética  
+necessita de pelo menos dois números
+
+        8--11=R
+(2, 5, 8, 11)
+a1 a2 a3 a4
+   a2--a3=n
+R razão=an-an-1
+an=
+
+a1=a1+0*R
+a2=a2+1*R
+a3=a1+2*R
+
+Ex..
+(-2, -5, -8,... -11) caso dê um valor diferente não é progressão aritmética, pode ser uma progressão geométrica
+R=-5-(-2)=-3
+
+progressão sem [...] é uma progressão finita, já uma com [...] é infinita. {crescente ou decrescente}
+
+sempre que:
+1) Se R> 0, então a p.a é crescente                           #############################|Fórmula do termo geral de uma PA|{an=a1+(n-1)*r}######################################
+
+
+1) em qualquer p.a; cada termo, exceto os extremos, é a média aritimética entre o precedente e o consequente
+
+ak=(ak-1+ak+1/2)
+
+
+verifique se a sequência (6, 13, 20, 27, 34) é uma pa
+ 6--13=r7
+(6,13,20,27,34)
+
+(x-4y, x-2y,x ,x+2y)
+r=x-2y-(-x-4y)
+r=x-2y-x+4y
+r=2y
+
+r=x-(x-2y)
+r=x-x+2y
+r=2y
+
+(2, 7/3,...)
+r=7/3-2/1=1/3   |mmc| 7-6/3=1/3
+
+7/3+1/3=8/3
+
+a2
+a3=a2+R
+a3=7/3+1/3
+a3=7+1/3+8/3
+
+an=2+9*1/3=2+9/3=2+3=5
+
+conta gotas 
+(4, 8, 12..100)
+
+an=100
+an=a1+(n-1)*R
+100=4+(n-1)*4
+100-4=(n-1)
+96=4*n-4
+96+4=4*n
+n=100/4=25
+a=25
+
+bichos
+R=4
+an=19+(n-1)*4
+an=19+4n-4
+an=13+4n
+492=(19+15+4n)*n/2
+914=(39+4n)n
+914=314+4n^2
+2n^2+17n-492=0
+
+=======
+R=42
+a1=42
+an=2142
+
+an=a1+(n-1)*r
+2142=42+(n-1)*42
+2142=42=42n-42
+n=2142/42=51
+
+a1=21
+R=4
+n=12
+a12=21+(12-1)*4
+a12=21+44=65
+s=(a1+a2)*2
+s=(21+65)/2*12=516
+total=516+42=558
+
+a1=5
+R=5
+
+"""
