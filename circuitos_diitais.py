d5943c3bf9c4c7f4cdd4efd4957758cdd4800997
Luan Victor <106849037+LottaFeroce@users.noreply.github.com>
2025-05-09 20:50:45 -0400

    Update circuitos_diitais.py
    tabelas_verdade_completas_mais_treino

--- circuitos_diitais.py
+++ circuitos_diitais.py
@@ -22,7 +22,10 @@ Null law|0*A = 0| 1 + A = 1
 Idempotent law|A*A = A |A + A = A
 Inverse law|A*¬A = 0|A+¬A=1
 Commutative law|A*B=BA|A+B=B+A
-Associative law| (Slide passou)
+Associative law|(AB)*C =A*(BC)|(A+B)+C = A+(B+C)
+Distributive law|A+BC = (A+B)*(A+C)|A(B+C=AB+AC
+Aborption law|A*(A+B)=A|A+AB=A
+De Morgan's law|¬A¬B=¬A+¬B|¬A+¬B=¬A¬B
 
 
 AND ex: A1*B0 = X0
@@ -32,11 +35,11 @@ NOR ex: 1+0 =  X0 negado -> 1 Res
 
 
 Tabela Verdade AND        Tabela Verdade NAND
-A|B|X  x(saida)           A|B|X  x(saida)
-0|0|0                     0|0|1b
-0|1|0                     0|1|1
-1|0|0                     1|0|1
-1|1|1                     1|1|0
+A|B|X|A¬B  x(saida)           A|B|X  x(saida)
+0|0|0|0                     0|0|1
+0|1|0|0                     0|1|1
+1|0|0|1                     1|0|1
+1|1|1|0                     1|1|0
 
 Tabela Verdade OR         Tabela Verdade NOR
 A|B|X  x(saida)           A|B|X  x(saida)
@@ -45,5 +48,31 @@ A|B|X  x(saida)           A|B|X  x(saida)
 1|0|1                     1|0|0
 1|1|1                     1|1|0
 
+Tabela Verdade XOR         Tabela Verdade XNOR
+A|B|X  x(saida)           A|B|X  x(saida)
+0|0|0                     0|0|1
+0|1|1                     0|1|0
+1|0|1                     1|0|0
+1|1|0                     1|1|0
+XOR(Só um lado pode ser verdadeiro AKA 1)
 
+ABCD
+0000 1
+0001 1 
+0010 2 
+0011 3
+0100 4
+0101 5
+0110 6
+0111 7
+1000 8
+1001 9
+1010 10
+1011 11
+1100 12
+1101 13
+1110 14
+1111 15
+
+Y = XOR (A,B) = Aº+B =¬AB +A¬B??????
 """
