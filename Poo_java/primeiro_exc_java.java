051019be1f3eee8c72432b8925627bebb844777f
Luan Victor <106849037+LottaFeroce@users.noreply.github.com>
2025-07-21 19:51:25 -0400

    Primeiro_semestre

---
+++ Poo_java/primeiro_exc_java.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    String name = "Thiongos";
+	    var numero = 28830;
+	    var numero_maluco = 2990.39401;
+	    var altura = 1.77f;
+	    
+		System.out.println("When the "+name+" is SUS!");
+		System.out.println("Hello World");
+		System.out.println("Número da tele-sena "+numero);
+		System.out.println("Número da ante-sena "+numero_maluco);
+		System.out.println("Sua altura é  "+altura);
+	}
+}
