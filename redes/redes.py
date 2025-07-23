d816b14bca44a2734a4658101944a0517f8d489d
Luan Victor <106849037+LottaFeroce@users.noreply.github.com>
2025-07-22 21:25:37 -0400

    Update redes.py

--- redes/redes.py
+++ redes/redes.py
@@ -4,4 +4,7 @@ broadcast é quando os octetos do host então maximizados (255)
 excemplos 192.168.0.0 ID
 192.168.0.0 Broadcast
 
+sempre menos 2(ID e Broadcast que são reservados) para fazer o calculo de quantas maquinas podem receber os IPs
+
+
 """
