181b03355ef80298774b48ee12e441167e72d7d2
Luan Victor <106849037+LottaFeroce@users.noreply.github.com>
2025-07-23 19:22:17 -0400

    Create Main.java

---
+++ Poo_java/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    var nome = "r'lyeh";
+	    var sobrenome ="o caido";
+	    var  cidade = "Insburn";
+	    var hobbie = "olhar para o abismo";
+	    System.out.printf("Olá %s %s você mora em %s e gosta de %s",nome,sobrenome,cidade,hobbie);
+	    System.out.print("\n");
+	    
+	    Scanner inper = new Scanner(System.in);
+	    System.out.println("Digite who shall be the chosen one for the deep ones: ");
+	    var pessoa = inper.next();
+	    System.out.println(pessoa+" ,will be  graced by the abyss");
+	    
+	   /*
+	    String name = "Thiongos";
+	    var numero = 28830;
+	    var numero_maluco = 2990.39401;
+	    var altura = 1.77f;
+	    //SusGaon//
+	    
+	    var nome = "hiperbole";
+	    var idade = 011010;
+	    System.out.printf("Olá %s você é muito hhhhhhhhhhhhhhhhhhhhh e tem %d anos",nome,idade);
+	    */ 
+	    /*Printar Inteiros %d
+	      Printar Strings %s
+	      Printar double %f
+	   
+		System.out.println("When the "+name+" is SUS!");
+		System.out.println("Hello World");
+		System.out.println("Número da tele-sena "+numero);
+		System.out.println("Número da ante-sena "+numero_maluco);
+		System.out.println("Sua altura é  "+altura); 
+		*/
+	}
+}
