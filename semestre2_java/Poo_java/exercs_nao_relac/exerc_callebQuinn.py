0ca8be9594d8d4d06871734b8cf31f264182fbb5
Luan Victor <106849037+LottaFeroce@users.noreply.github.com>
2025-10-08 21:33:43 -0400

    Create exerc_callebQuinn.py

---
+++ semestre2_java/Poo_java/exercs_nao_relac/exerc_callebQuinn.py
@@ -0,0 +1,25 @@
+class Aluno:
+    def __init__(self, nome, matricula, notas):
+        self.nome = nome
+        self.matricula = matricula
+        self.notas = notas
+
+    def calcular_media(self):
+        if len(self.notas) == 0:
+            return 0
+        return sum(self.notas) / len(self.notas)
+
+    def mostrar_situacao(self):
+        media = self.calcular_media()
+        if media >= 7:
+            return f"APROVADO (média = {media:.2f})"
+        elif media >= 5:
+            return f"EXAME (média = {media:.2f})"
+        else:
+            return f"REPROVADO (média = {media:.2f})"
+
+aluno1 = Aluno("Karlmarcos", "RA12345", [8.0, 7.0, 9.0, 6.0])
+
+print(aluno1.nome)
+print(aluno1.calcular_media())
+print(aluno1.mostrar_situacao())
