8b5a6c29a740e8e0c48bff28d70010d8f9305971
Luan Victor <106849037+LottaFeroce@users.noreply.github.com>
2025-11-10 20:15:19 -0400

    iaiton

--- semestre2_java/Poo_java/lista3/crud_no_xamp/Cliente.py
+++ semestre2_java/Poo_java/lista3/crud_no_xamp/Cliente.py
@@ -15,39 +15,22 @@ class Cliente:
     
     def buscar(self):
         self.db = Database()
-        cliente = self.db.select()
-        return cliente
+        dados = self.db.select()
+        return dados
     
     def buscar_por_id(self,id):
         self.db = Database()
         cliente = self.db.select_by_id(id)
         return cliente
-
+    
     def atualizar(self,tupla):
         self.db = Database()
         cliente = self.db.update(tupla)
         return cliente
- 
+    
+    
     def excluir(self,id):
         self.db = Database()
         result = self.db.delete(id)
         return result
-    
 
-
-cli = Cliente()
-clientes = cli.buscar()
-
-for item in clientes:
-    print(item)
-
-id_atualizar = int(input("Digite o ID do cliente que deseja atualizar: "))
-cli_atualizar = cli.buscar_por_id(id_atualizar)
-cli_atualizar = list(cli_atualizar)
-
-cli_atualizar[1] = input("Digite o novo nome")
-cli_atualizar[2] = input("Digite o novo cpf")
-cli_atualizar[3] = input("Digite o novo fone")
-cli_atualizar[4] = input("Digite o novo cidade")
-
-print(cli_atualizar)
