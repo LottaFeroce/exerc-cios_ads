018821d0b2dcccadc05e9df85a32efe52829570e
LottaFeroce <sonavitae@gmail.com>
2026-04-23 19:05:04 -0400

    Update mysqlconn.py

--- semestre2_java/Poo_java/gestao/mysqlconn.py
+++ semestre2_java/Poo_java/gestao/mysqlconn.py
@@ -1,2 +1,44 @@
-import mysqlConnector
-import mysql
+import mysql.connector
+conexao = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = ""
+)
+
+#verify
+if conexao.is_connected():
+    print("\nConexão efetuada com exito")
+
+#conexao.cursor().execute()
+cursor = conexao.cursor()
+cursor.execute("CREATE DATABASE IF NOT EXISTS aula_connect")
+
+conexao.database = "aula_connect"
+
+cursor.execute("Show databases")
+
+#show databases
+for banco in cursor.fetchall():
+    print(banco)
+
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS alunos(
+        ID_aluno int primary key auto_increment,
+        nome varchar(100) not null,
+        idade int)'''
+    )
+#Insert
+sql = "INSERT INTO alunos(nome,idade) VALUES (%s, %s)"
+values = ("Hokma", 60)
+cursor.execute(sql, values)
+#commit para enviar ao banco
+conexao.commit()
+
+cursor.execute("SELECT * FROM alunos")
+results = cursor.fetchall()
+for linha in results:
+    print(linha)
+
+#close
+cursor.close()
+conexao.close()
