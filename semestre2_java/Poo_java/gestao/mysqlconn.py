e686427ff830f15338cc44843f04f3d441c5a7cf
LottaFeroce <sonavitae@gmail.com>
2026-04-23 21:38:16 -0400

    Update mysqlconn.py

--- semestre2_java/Poo_java/gestao/mysqlconn.py
+++ semestre2_java/Poo_java/gestao/mysqlconn.py
@@ -15,13 +15,12 @@ cursor.execute("CREATE DATABASE IF NOT EXISTS aula_connect")
 
 conexao.database = "aula_connect"
 
-cursor.execute("Show databases")
-
-#show databases
+'''cursor.execute("Show databases")
+show databases
 for banco in cursor.fetchall():
-    print(banco)
+    print(banco)'''
 
-    cursor.execute(
+cursor.execute(
         '''CREATE TABLE IF NOT EXISTS alunos(
         ID_aluno int primary key auto_increment,
         nome varchar(100) not null,
@@ -34,10 +33,24 @@ cursor.execute(sql, values)
 #commit para enviar ao banco
 conexao.commit()
 
+#sql_update = "UPDATE alunos set idade =%s where nome = %s" 
+sql_update = "UPDATE `aula_connect`.`alunos` SET `nome` = 'cleb' WHERE (`ID_aluno` = '1');"
+cursor.execute(sql_update)
+conexao.commit()
+
 cursor.execute("SELECT * FROM alunos")
 results = cursor.fetchall()
 for linha in results:
     print(linha)
+
+'''sql_del = "delete from alunos where id=%s"
+valores_del = (1,)
+cursor.execute(sql_del, valores_del)
+conexao.commit()
+'''
+sql_delete = "DELETE FROM aula_connect.alunos WHERE ID_aluno IN (%s, %s)"
+cursor.execute(sql_delete, (2, 3))
+conexao.commit()
 
 #close
 cursor.close()
