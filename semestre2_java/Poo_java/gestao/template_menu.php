d282c6d9941b2cb75bf1c9f8a0b3733f4789a732
LottaFeroce <sonavitae@gmail.com>
2026-05-07 20:40:43 -0400

    Create template_menu.php

---
+++ semestre2_java/Poo_java/gestao/template_menu.php
@@ -0,0 +1,7 @@
+<?php $title_page = "Menu Template";?>
+<nav class="menu_pagina-1">
+    <a href="#" class="aba-1 ativo-1">Dashboard</a>
+    <a href="#" class="aba-1">Veículos</a>
+    <a href="#" class="aba-1">Solicitações</a>
+    <a href="#" class="aba-1">Corridas</a>
+</nav>
