539871b9842b13ed5b785b4259fae10753d20d0d
LottaFeroce <sonavitae@gmail.com>
2026-06-11 20:41:26 -0400

    Csv do kaggle providenciado pelo docente
    Trágico

--- AV_thiongo/carrinho.py
+++
@@ -1,6 +0,0 @@
-class Carrinho_item:
-    def __init__(self, id_cliente, id_produto, quantidade, valor):
-        self.id_cliente = id_cliente
-        self.id_produto = id_produto
-        self.quantidade = quantidade
-        self.valor = valor
--- semestre2_java/Poo_java/gestao/db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-def conectar():
-    conexao = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "",
-    database = "sistema_alunos"
-    )
-    return conexao
